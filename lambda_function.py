8abedc853a5f32c6bf815d30286f9e3b2a572b87
ankitkumar-devops <119074004+ankitkumar-devops@users.noreply.github.com>
2023-11-27 15:59:38 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! version 7')
+        'body': json.dumps('Hello from Lambda! version 9')
     }
 
