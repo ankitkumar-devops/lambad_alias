7704cbbf7b5dc37bb79f6012fb5e7900f060c60a
ankitkumar-devops <119074004+ankitkumar-devops@users.noreply.github.com>
2023-11-29 13:34:45 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! thehhlakfd versi hjhasdjfl thiso sdafdsg fdij sjafdaa;h thsllfdk apdson 9')
+        'body': json.dumps('Hello from Lambda!  form testing lambda-janitor')
     }
 
